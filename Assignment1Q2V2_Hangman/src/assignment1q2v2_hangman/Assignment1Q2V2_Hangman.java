9c3a737e599cdba4370eb342f77dee1aabda2803
Rebecca <beckylum0214@gmail.com>
2017-09-05 03:24:57 +0800

    first commit

---
+++ Assignment1Q2V2_Hangman/src/assignment1q2v2_hangman/Assignment1Q2V2_Hangman.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment1q2v2_hangman;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author rebecca
+ */
+public class Assignment1Q2V2_Hangman {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        char theFlag = 'y';
+        Scanner theKB = new Scanner(System.in);
+        int theTries;
+        
+        do{
+            Hangman gameOne = new Hangman("the fox");
+            System.out.println("Let's play a round of hangman");
+            System.out.println("We are playing hangman.");
+            theTries = 0;
+            
+            do
+            {   
+                gameOne.getDisguisedWord();
+                theTries = theTries + 1;
+                gameOne.setGameTries(theTries);
+                gameOne.makeGuess();
+            }while(!gameOne.isFound());
+            
+            System.out.println("Congratulations, you found the secret word: "
+            + gameOne.getGameStr());
+            
+            Hangman gameTwo = new Hangman("jazz");
+            System.out.println("Let's play another round of hangman");
+            System.out.println("We are playing hangman.");
+            theTries = 0;
+            do
+            {   
+                gameTwo.getDisguisedWord();
+                
+                gameTwo.makeGuess();
+                
+                theTries = theTries + 1;
+                gameTwo.setGameTries(theTries);
+                
+                System.out.println("Correct. Guesses made "+ 
+                                gameTwo.getGameTries() + " with " 
+                                + gameTwo.getGameWrong()+ " wrong.");
+            }while(!gameTwo.isFound());
+            
+            System.out.println("Congratulations, you found the secret word: "
+            + gameTwo.getGameStr());
+            
+            Hangman gameThree = new Hangman("quick");
+            System.out.println("Let's play the final round of hangman");
+            System.out.println("We are playing hangman.");
+            theTries = 0;
+            do
+            {   
+                gameThree.getDisguisedWord();
+                
+                gameThree.makeGuess();
+                
+                theTries = theTries + 1;
+                gameThree.setGameTries(theTries);
+                
+                System.out.println("Correct. Guesses made "+ 
+                                gameThree.getGameTries() + " with " 
+                                + gameThree.getGameWrong()+ " wrong.");
+            }while(!gameThree.isFound());
+            
+            System.out.println("Congratulations, you found the secret word: "
+            + gameThree.getGameStr());
+            
+            System.out.println("Would you like to continue? (Y/N)");
+            theFlag = theKB.next().charAt(0);
+            
+        }while(theFlag == 'y');
+    }
+    
+}
---
+++ Assignment1Q2V2_Hangman/src/assignment1q2v2_hangman/Hangman.java
@@ -0,0 +1,269 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment1q2v2_hangman;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * This class contains exclusively methods that operate or support the running
+ * of the hangman program
+ * @author rebecca
+ */
+public class Hangman {
+    
+    private String gameStr;
+    private StringBuilder gameAns = new StringBuilder();
+    private int gameTries;
+    private int gameWrong;
+    private String gameSet;
+   
+    Scanner theKB = new Scanner(System.in);
+    private Random theRnd = new Random(); //to generate a psudorandom number
+    private int rndNum;
+    
+    /**
+     * the constructor takes in an input string 
+     * @param inputStr
+     */
+    public Hangman(String inputStr)
+    {
+        this.gameStr = inputStr;
+        this.gameSet = this.gameSetOps();
+        this.gameTries = 0;
+        this.rndNum = theRnd.nextInt();
+    }
+
+    public int getGameTries() {
+        return gameTries;
+    }
+    
+    public String getGameStr() {
+        return gameStr;
+    }
+
+    public StringBuilder getGameAns() {
+        return gameAns;
+    }
+
+    public int getGameWrong() {
+        return gameWrong;
+    }
+    
+    
+    public void StudentInfo()
+    {
+        System.out.println("Name:               Rebecca Lim");
+        System.out.println("Student Number:     33111264");
+        System.out.println("Enrolment:          Internal");
+        System.out.println("Tutor:              Mark Abernethy");
+        System.out.println("Tutorial:           Thursday, 10.30am");
+    }
+    
+    /**
+     * This method takes in a set word through the contructor, 
+     * checks it against the answer and masks the rest. 
+     */
+    public void makeGuess()
+    {
+        String tempAns;      
+      
+        System.out.println("Guess a letter:");
+        tempAns = theKB.nextLine().toLowerCase();
+        //tempTest = Character.toString(tempAns);
+
+        if(tempAns.isEmpty())
+        {
+            System.out.println("You have entered an empty string. "
+                    + "Please enter something!");
+        }
+        else 
+            if (tempAns.length() > 1 )
+            {
+                System.out.println("Sorry, bad guess. Need a single letter.");
+            }
+            else
+                if(Character.isDigit(tempAns.charAt(0)))
+                {
+                    System.out.println("Sorry, your guess must be an alphabet "
+                            + "character from a to z.");
+                }
+                else
+                {
+                    if((this.gameStr.contains(tempAns)))
+                    {
+                        if(this.gameAns.toString().contains(tempAns))
+                        {
+                            System.out.println("You have made this guess already!");
+                        }
+                        else
+                        {
+                            this.gameAns.append(tempAns);
+                            System.out.println("Correct. Guesses made "+ 
+                                    this.getGameTries() + " with " 
+                                    + this.getGameWrong()+ " wrong.");
+                        }
+                    }
+                    else
+                    {
+                        gameWrong = gameWrong + 1;
+                        System.out.println("Incorrect. Guesses made "+ 
+                                    this.getGameTries() + " with " 
+                                    + this.getGameWrong()+ " wrong.");
+                    }
+
+                }
+    }    
+       
+    
+    /**
+     * This method takes in a set word through the contructor 
+     * and masks each letter in the word with questions marks for the user
+     * to guess. 
+     */
+    public void getDisguisedWord()
+    {
+        boolean found = false;
+        int j = 0;
+        
+        System.out.print("The disguised word is <");
+        
+        if(this.gameAns.length() == 0)
+        {
+            for(int i = 0; i < this.gameStr.length(); i +=1)
+            {
+                System.out.print("?");
+            }
+
+        }
+        else
+        {
+            
+            for(int i = 0; i < this.gameStr.length(); i++)
+            {
+                while(found == false && j < this.gameAns.length())
+                {
+                    if(this.gameAns.charAt(j) == this.gameStr.charAt(i))
+                    {
+                        System.out.print(this.gameStr.charAt(i));
+                        found = true;
+                    }
+                    j++;
+                }
+
+                if (found == false)
+                {
+                    System.out.print("?");
+                }
+                found = false;
+                j = 0;
+            }
+            
+            
+        }
+            
+        System.out.print("> \n");
+    }
+
+    public void setGameTries(int gameTries) {
+        this.gameTries = gameTries;
+    }
+
+    public void setGameWrong(int gameWrong) {
+        this.gameWrong = gameWrong;
+    }
+    
+    // Second attempt at masking the method does not 
+    /*
+    public void getDisguisedWord2()
+    {
+        StringBuilder theMask = new StringBuilder();
+        int showIndex;
+        
+       
+        for(int i = 0; i < this.gameStr.length(); i +=1)
+        {
+            theMask.append("?");
+        }
+        if(this.gameAns.length() > 0)
+        {
+            for (int i = 0; i < this.gameAns.length(); i++)
+            {
+                showIndex = this.gameStr.indexOf(this.gameAns.charAt(i));
+                theMask.setCharAt(showIndex, this.gameAns.charAt(i));
+            }
+            
+        }
+        
+        System.out.printf("The disguised word is <%s> \n", theMask);
+    }
+    */
+    
+    /**
+     * This method compares the hashed answer and hashed secret word to return
+     * whether the answer has been found.
+     * @return theFlag 
+     */
+    public boolean isFound()
+    {
+        boolean theFlag = false;
+        int hashedStr;
+        int hashedAns;
+        hashedStr  = HashItem(this.gameSet);
+        hashedAns = HashItem(this.gameAns.toString());
+        if(hashedStr == hashedAns)
+        {
+            theFlag = true;
+        }
+               
+        return theFlag;
+    }
+    
+    /**
+     * 
+     * @param inputStr
+     * @return
+     */
+    public int HashItem(String inputStr)
+    {
+        int hashTot = 0;
+        int hash  = 0;
+        
+        char str;
+        
+        
+        for(int i = 0; i < inputStr.length(); i+=1)
+        {
+            str = inputStr.charAt(i);
+            hash =  rndNum * str;
+            hashTot += hash;
+        }
+        return hashTot;
+    }
+    
+    /**
+     *  This method returns the secret word for comparison without duplicate 
+     *  characters. This method would create pseudo-set capabilities for the class
+     * @return
+     */
+    public String gameSetOps()
+    {
+        String setResults = "";
+        for(int i = 0; i < this.gameStr.length(); i+=1)
+        {
+            if(!setResults.contains(String.valueOf(this.gameStr.charAt(i))))
+            {
+                setResults += String.valueOf(this.gameStr.charAt(i));
+            }
+        }
+        return setResults;
+    }
+    
+    
+    public void setGameAns(StringBuilder gameAns) {
+        this.gameAns = gameAns;
+    }
+}
